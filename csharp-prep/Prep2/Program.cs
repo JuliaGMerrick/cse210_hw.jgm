be685901e54063a6cea5d4d35875e9178c184d7f
JuliaGMerrick <98558469+JuliaGMerrick@users.noreply.github.com>
2023-07-15 18:55:55 -0400

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade: ");
+        string grade = Console.ReadLine();
+        
+        int x = int.Parse(grade);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+        else if (x <= 89 && x >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+        else if (x <= 79 && x >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (x <= 69 && x >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade is an F.");
+        }
     }
 }
