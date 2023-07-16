10afd2b460cd7d1e51f6d5d00dedf82ecdfb94f4
JuliaGMerrick <98558469+JuliaGMerrick@users.noreply.github.com>
2023-07-16 17:33:06 -0400

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        
+        int guess = -1
+        while (guess != magicNumber)
+        {
+           Console.Write("What's the magic number? ")
+           guess = int.Parse(Console.ReadLine()) 
+
+           if (guess > magicNumber)
+           {
+            Console.Write("Lower.")
+           }
+
+           else if (guess < magicNumber)
+           {
+            Console.Write("Higher.")
+           }
+
+           else
+           {
+            Console.Write("You got it! ")
+           }
+        }
     }
 }
