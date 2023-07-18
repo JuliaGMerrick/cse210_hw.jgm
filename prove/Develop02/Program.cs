45fa62a9048a09e52a5a97c735684b984b1ccdbd
JuliaGMerrick <98558469+JuliaGMerrick@users.noreply.github.com>
2023-07-18 11:38:27 -0400

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        class Journal 
+        {
+            public void Run() 
+            {
+                DisplayIntro();
+
+                DisplayOutro();
+            }
+
+            private void CreateJounalFile() {}
+
+            private void DisplayIntro() 
+            {
+                ForegroundColor = ConsoleColor.Black;
+                BackgroundColor = ConsoleColor.White;
+                Clear();
+                Console.WriteLine('Welcome to the journal app. ');
+                Console.ReadKey(true);
+            }
+            
+            private void DisplayOutro() 
+            {
+                Console.WriteLine('Thank you for using the journal app. ');
+                Console.ReadKey(true);
+            }
+
+            private void DisplayJournalContents() {}
+
+            private void AddEntry() {}
+        }
+            
     }
 }
